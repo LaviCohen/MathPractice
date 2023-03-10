320c08a64ce756e5483ea3f5f6d0118ed5001b4e
lavi cohen <lavicohen78@gamil.com>
2023-03-10 13:23:02 +0200

    Opening Animation
    Adding opening animation in 'MainActivity'

--- app/src/main/java/com/example/mathpractice/activities/MainActivity.java
+++ app/src/main/java/com/example/mathpractice/activities/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.mathpractice.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mathpractice.R;
 import com.example.mathpractice.activities.practice.PracticeActivity;
@@ -15,6 +20,27 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		startActivity(new Intent(this, PracticeActivity.class));
+		ImageView imageView = findViewById(R.id.ivAppLogo);
+		ObjectAnimator ivAnimatorTransX = ObjectAnimator.ofFloat(imageView, "translationX", 0f);
+		ObjectAnimator ivAnimatorAlpha = ObjectAnimator.ofFloat(imageView, "Alpha", 1f);
+		ivAnimatorAlpha.setDuration(5000);
+		ivAnimatorTransX.setDuration(5000);
+
+		TextView textView = findViewById(R.id.tvSubText);
+		ObjectAnimator tvAnimatorTransY = ObjectAnimator.ofFloat(textView, "translationY", 0f);
+		ObjectAnimator tvAnimatorAlpha = ObjectAnimator.ofFloat(textView, "Alpha", 1f);
+		tvAnimatorAlpha.setDuration(5000);
+		tvAnimatorTransY.setDuration(5000);
+		Button button = findViewById(R.id.bLetsGo);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(MainActivity.this, PracticeActivity.class));
+			}
+		});
+		ivAnimatorAlpha.start();
+		ivAnimatorTransX.start();
+		tvAnimatorAlpha.start();
+		tvAnimatorTransY.start();
 	}
 }
